444faa10d0371e530e43113ced32157660665aff
MostafaSamer <mostafasamer1999@hotmail.com>
2018-12-17 19:51:40 +0200

    Still Coding

---
+++ src/GUI_fx/logIn.java
@@ -0,0 +1,16 @@
+package GUI_fx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class logIn extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
