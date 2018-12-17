024b6ebbd96ad05d48bbbd3f64cb0635a82879bb
MostafaSamer <mostafasamer1999@hotmail.com>
2018-12-18 00:48:05 +0200

    Still Coding

--- src/GUI_fx/LogIn.java
+++ src/GUI_fx/LogIn.java
@@ -1,5 +1,32 @@
 package GUI_fx;
 
-public class LogIn {
+import javafx.application.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.*;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class LogIn extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("LogIn.fxml"));
+            Scene scene = new Scene(root, 600, 400);
+            stage.setTitle("Log In");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println("File Not Found");
+        }
+
+    }
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,8 +1,12 @@
 package Main;
+import GUI_fx.*;
+import javafx.application.Application;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Application.launch(LogIn.class, args);
 
     }
 
