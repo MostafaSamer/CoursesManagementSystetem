3126059e5bf91c9c541ad68316461a4e25a9928a
MostafaSamer <mostafasamer1999@hotmail.com>
2018-12-20 01:08:25 +0200

    Still Coding

--- src/GUI_Imp/logIN.java
+++ src/GUI_Imp/logIN.java
@@ -15,9 +15,11 @@ public class logIN {
             return true;
         } else if (y.Login(ID, pass)) {
             // Start Student
+            System.out.println("Student Entered");
             return true;
         } else if (z.Login(ID, pass)) {
             //Start Instractor
+            System.out.println("Instractor Entered");
             return true;
         }
         return false;
