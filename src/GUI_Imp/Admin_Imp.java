40a7b87e2cf383cc8dce40acc0a404dd988339ce
Mostafa Samer <mostafasamer1999@hotmail.com>
2018-12-22 19:34:52 +0200

    Still Coding

--- src/GUI_Imp/Admin_Imp.java
+++ src/GUI_Imp/Admin_Imp.java
@@ -4,19 +4,23 @@ import Main.*;
 public class Admin_Imp {
 
     public boolean addStudent(int id, String name, String pass, int age, double ssn) {
-        if (new Student(name, id, pass, ssn, age).AddStudent()) {
+        if (cheackID(id) && new Student(name, id, pass, ssn, age).AddStudent()) {
             return true;
         }
         return false;
     }
 
     public boolean addInstractor(int id, String name, String pass, int age, double ssn, double salary) {
-        if (new Instractor(name, id, pass, ssn, age, salary).AddInstractor()) {
+        if (cheackID(id) && new Instractor(name, id, pass, ssn, age, salary).AddInstractor()) {
             return true;
         }
         return false;
     }
 
-    //TODO make sure that the id of the new person is not used in the other modeuel
+    public boolean cheackID(int id) {
+        if(new Student().searchStudent(id).getAge() == 0 && new Instractor().searchInstractor(id).getAge() == 0) {
+            return true;
+        } return false;
+    }
 
 }
