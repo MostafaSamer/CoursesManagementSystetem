b8b36993ec5f3134517c4f01de40393fff3cf98d
MostafaSamer <mostafasamer1999@hotmail.com>
2019-01-11 21:04:31 +0200

    Still Coding

--- src/Main/Admin.java
+++ src/Main/Admin.java
@@ -3,9 +3,6 @@ package Main;
 public class Admin extends Person {
     @Override
     public boolean Login (int ID , String pass) {
-
-       
         return (ID == 123 && pass.equals("123"));
-
     }
 }
